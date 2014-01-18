0eceb63371166635c7f1cb832aaf5dd24533be1b
Alex.F <gfreezy@gmail.com>
2014-01-18 18:10:36 +0800

    memory allocation

--- main.c
+++ main.c
@@ -1,92 +1,58 @@
-//
-//  main.c
-//  linux
-//
-//  Created by Chao Fei on 12/18/13.
-//  Copyright (c) 2013 Chao Fei. All rights reserved.
-//
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/uio.h>
-#include <setjmp.h>
 #include "tlpi_hdr.h"
 
+#define MAX_MALLOCS 1000000
 
-extern char **environ;
-
-
-int setenv(const char *name, const char *value, int overwrite)
-{
-    char *old_value = getenv(name);
-    if (old_value && !overwrite) {
-        return 0;
-    }
-    char *name_ = strdup(name);
-    char *value_ = strdup(value);
-    
-    size_t size = strlen(name) + strlen(value) + 2;
-    char *new = malloc(size);
-    if(snprintf(new, size, "%s=%s", name_, value_) < 0)
-    {
-        errExit("snprintf");
-    }
-    return putenv(new) < 0 ? -1 : 0;
+void 
+*malloc_(size_t size) {
+    printf("current programming break is %10p\n", sbrk(0));
+    return malloc(size);
 }
 
-int unsetenv(const char* name) 
-{
-    char **old_env = environ;
-    environ = NULL;
+int
+main(int argc, char *argv[]) {
+    char *ptr[MAX_MALLOCS];
+    int freeStep, freeMin, freeMax, blockSize, numAllocs;
     
-    size_t env_size = 0;
-    char **ep, **ep2;
-    int ret;
-    for(ep=old_env; *ep != NULL; ep++)
-    {
-        env_size++;
-    }
-
-    char **env = malloc(env_size * sizeof(*old_env));
-    for(ep=old_env, ep2=env; *ep != NULL; ep++, ep2++)
-    {
-        *ep2 = *ep;
+    printf("\n");
+    if (argc < 3 || strcmp(argv[1], "--help") == 0) {
+        usageErr("%s num-allocs block-size [step [min [max]]]\n", argv[0]);
     }
     
-    for(ep=env; *ep!=NULL; ep++)
-    {
-        if(strncmp(name, (const char*)*ep, strlen(name)) != 0)
-        {
-            if(putenv(strdup(*ep)) != 0)
-            {
-                return -1;
-            }
+    numAllocs = getInt(argv[1], GN_ANY_BASE | GN_GT_0, "num-allocs");
+    if (numAllocs > MAX_MALLOCS) {
+        cmdLineErr("num-allocs > %z\n", MAX_MALLOCS);
+    }
+    
+    blockSize = getInt(argv[2], GN_ANY_BASE | GN_GT_0, "block-size");
+    freeStep = argc > 3 ? getInt(argv[3], GN_ANY_BASE | GN_GT_0, "step") : 1;
+    freeMin = argc > 4 ? getInt(argv[4], GN_GT_0, "min") : 1;
+    freeMax = argc > 5 ? getInt(argv[5], GN_GT_0, "max") : numAllocs;
+    
+    if (freeMax > numAllocs) {
+        cmdLineErr("free-max > num-allocs\n");
+    }
+    
+    printf("Initial program break:             %10p\n", sbrk(0));
+    printf("Allocating %d*%d bytes\n", numAllocs, blockSize);
+    
+    int i;
+    for (i = 0; i < numAllocs; i++) {
+        ptr[i] = malloc_(blockSize);
+        if (ptr[i] == NULL) {
+            errExit("malloc");
         }
     }
-    free(env);
-    return 0;
-}
+    
+    printf("Programming break is now:       %10p\n", sbrk(0));
+    
+    printf("Free blocks from %d to %d in steps of %d\n",
+            freeMin, freeMax, freeStep);
 
-
-void print_environ()
-{
-    char **ep;
-    for(ep=environ; *ep!=NULL; ep++)
-    {
-        printf("%s\n", *ep);
+    for (i = freeMin -1; i < freeMax; i+=freeStep) {
+        free(ptr[i]);
     }
-}
-
-int main(int argc, const char * argv[])
-{
-    print_environ();
     
-    printf("after set environ\n");
-    setenv("en1", "va1", 0);
-    setenv("en1", "va2", 1);
-    print_environ();
+    printf("After free, programming break is %10p\n", sbrk(0));
     
-    printf("after clear\n");
-    unsetenv("en1");
-    print_environ();
     exit(EXIT_SUCCESS);
 }
